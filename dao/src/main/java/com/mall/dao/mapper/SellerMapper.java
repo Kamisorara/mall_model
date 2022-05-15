5e9c12b70bf4b4f966f155f8ead949961512ecd7
kamisora <1210281722@qq.com>
2022-05-15 14:09:47 +0800

    已完成模块化设计（已修复打包bug）

---
+++ dao/src/main/java/com/mall/dao/mapper/SellerMapper.java
@@ -0,0 +1,8 @@
+package com.mall.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mall.entity.entity.shop.Seller;
+
+public interface SellerMapper extends BaseMapper<Seller> {
+
+}
---
+++ entity/src/main/java/com/mall/entity/entity/shop/Seller.java
@@ -0,0 +1,35 @@
+package com.mall.entity.entity.shop;
+
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * (Seller)表实体类
+ *
+ * @author Kamisora
+ * @since 2022-05-15 13:19:29
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName(value = "shop_seller")
+public class Seller {
+    //卖家店铺id
+    @TableId
+    private Long id;
+    //卖家店铺名称
+    private String sellerName;
+    //描述分
+    private Double sellerMarl1;
+    //服务分
+    private Double sellerMark2;
+    //物流分
+    private Double sellerMark3;
+
+}
+
