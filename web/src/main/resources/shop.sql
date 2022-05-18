5fbacb09af9970b864b175fe39a5a0b746b6d566
kamisora <1210281722@qq.com>
2022-05-18 15:48:37 +0800

    个人信息界面设计基本完成

--- web/src/main/resources/shop.sql
+++ web/src/main/resources/shop.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : Linux数据库
+ Source Server         : linux数据库本地
  Source Server Type    : MySQL
  Source Server Version : 80028
- Source Host           : 192.168.31.100:3306
+ Source Host           : 192.168.36.100:3306
  Source Schema         : shop
 
  Target Server Type    : MySQL
  Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 15/05/2022 13:33:46
+ Date: 18/05/2022 15:48:13
 */
 
 SET NAMES utf8mb4;
@@ -40,7 +40,7 @@ CREATE TABLE `shop_classification`  (
   `classification_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品分类名称',
   `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '商品图标element',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of shop_classification
@@ -70,13 +70,15 @@ CREATE TABLE `shop_commodities`  (
   `photo2` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '副照片2',
   `photo3` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '副照片3',
   `photo4` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '副照片4',
+  `seller_id` int NOT NULL COMMENT '销售店家id',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of shop_commodities
 -- ----------------------------
-INSERT INTO `shop_commodities` VALUES (1, '官方', 'MSI/微星RTX 3060', 3599.00, '暂无优惠', '火星送往地球', 999, 999, 'https://img.alicdn.com/imgextra/i1/2114030042/O1CN01VxaRe41CBHNfA3wqa_!!2114030042.jpg_430x430q90.jpg', 'https://img.alicdn.com/imgextra/i4/2114030042/O1CN01aQzPdw1CBHGO1rFPo_!!2114030042.jpg_60x60q90.jpg', 'https://img.alicdn.com/imgextra/i1/2114030042/O1CN01cdBULA1CBHGUvLCnS_!!2114030042.jpg_60x60q90.jpg', 'https://img.alicdn.com/imgextra/i1/2114030042/O1CN01lyoFTB1CBHHsIC4bF_!!2114030042.jpg_60x60q90.jpg', 'https://img.alicdn.com/imgextra/i1/2114030042/O1CN01VxaRe41CBHNfA3wqa_!!2114030042.jpg_60x60q90.jpg');
+INSERT INTO `shop_commodities` VALUES (1, '官方', 'MSI/微星RTX 3060', 3599.00, '暂无优惠', '火星送往地球', 999, 999, 'https://img.alicdn.com/imgextra/i1/2114030042/O1CN01VxaRe41CBHNfA3wqa_!!2114030042.jpg_430x430q90.jpg', 'https://img.alicdn.com/imgextra/i4/2114030042/O1CN01aQzPdw1CBHGO1rFPo_!!2114030042.jpg_60x60q90.jpg', 'https://img.alicdn.com/imgextra/i1/2114030042/O1CN01cdBULA1CBHGUvLCnS_!!2114030042.jpg_60x60q90.jpg', 'https://img.alicdn.com/imgextra/i1/2114030042/O1CN01lyoFTB1CBHHsIC4bF_!!2114030042.jpg_60x60q90.jpg', 'https://img.alicdn.com/imgextra/i1/2114030042/O1CN01VxaRe41CBHNfA3wqa_!!2114030042.jpg_60x60q90.jpg', 1);
+INSERT INTO `shop_commodities` VALUES (2, '官方', 'RTX3090', 3333.33, '暂无优惠', '地球送往火星', 111, 111, 'https://img.alicdn.com/imgextra/i1/2114030042/O1CN01VxaRe41CBHNfA3wqa_!!2114030042.jpg_430x430q90.jpg', 'https://img.alicdn.com/imgextra/i1/2114030042/O1CN01VxaRe41CBHNfA3wqa_!!2114030042.jpg_430x430q90.jpg', 'https://img.alicdn.com/imgextra/i1/2114030042/O1CN01VxaRe41CBHNfA3wqa_!!2114030042.jpg_430x430q90.jpg', 'https://img.alicdn.com/imgextra/i1/2114030042/O1CN01VxaRe41CBHNfA3wqa_!!2114030042.jpg_430x430q90.jpg', 'https://img.alicdn.com/imgextra/i1/2114030042/O1CN01VxaRe41CBHNfA3wqa_!!2114030042.jpg_430x430q90.jpg', 2);
 
 -- ----------------------------
 -- Table structure for shop_noticeboard
@@ -86,7 +88,7 @@ CREATE TABLE `shop_noticeboard`  (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '公示表id',
   `notice` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '公式文字',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of shop_noticeboard
@@ -107,22 +109,23 @@ CREATE TABLE `shop_recommended`  (
   `price` decimal(10, 2) NULL DEFAULT NULL COMMENT '价格',
   `mark` int NULL DEFAULT NULL COMMENT '评论',
   `sell` int NULL DEFAULT NULL COMMENT '月销量',
+  `seller_id` int NOT NULL COMMENT '对应的店家id',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of shop_recommended
 -- ----------------------------
-INSERT INTO `shop_recommended` VALUES (1, 'MSI/微星RTX 3060', 'https://g-search3.alicdn.com/img/bao/uploaded/i4/i1/654519625/O1CN01L4xb8s2KyIt2aPRJL_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 3599.00, 999, 999);
-INSERT INTO `shop_recommended` VALUES (2, '七彩虹RTX3060/3060TI', 'https://g-search2.alicdn.com/img/bao/uploaded/i4/i4/3918712328/O1CN01Dgtlcp1T4GnUp8kXu_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 3499.00, 333, 444);
-INSERT INTO `shop_recommended` VALUES (3, '华硕ROG猛禽RTX3060', 'https://g-search3.alicdn.com/img/bao/uploaded/i4/i4/2210835472380/O1CN01VreFtP1TS5OMe5Eio_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 2849.00, 444, 444);
-INSERT INTO `shop_recommended` VALUES (4, '七彩虹RTX3080/', 'https://g-search1.alicdn.com/img/bao/uploaded/i4/i1/2133729733/O1CN01Bd2jO02LllfAvf7vU_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 9199.00, 333, 333);
-INSERT INTO `shop_recommended` VALUES (5, '影驰RTX3080/Ti', 'https://g-search1.alicdn.com/img/bao/uploaded/i4/i1/2651871058/O1CN01ytqlo91JgbmPyVHqN_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 9300.00, 444, 444);
-INSERT INTO `shop_recommended` VALUES (6, '七彩虹RTX3070/Ti', 'https://g-search1.alicdn.com/img/bao/uploaded/i4/i4/2133729733/O1CN01eYLZYr2Lllf7DLy2W_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 4599.00, 555, 555);
-INSERT INTO `shop_recommended` VALUES (7, '猛禽RTX3090/3090TI', 'https://g-search2.alicdn.com/img/bao/uploaded/i4/i2/654519625/O1CN01izYfVL2KyIt19oPoC_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 12999.00, 111, 1111);
-INSERT INTO `shop_recommended` VALUES (8, '英伟达NVIDIA公版无锁', 'https://g-search2.alicdn.com/img/bao/uploaded/i4/i1/3971128232/O1CN01MXBMol2AgJ67nn97y_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 7299.00, 111, 111);
-INSERT INTO `shop_recommended` VALUES (9, '影驰RTX3090名人堂', 'https://g-search2.alicdn.com/img/bao/uploaded/i4/i2/2681126335/O1CN01jwyrX21wfTpYDl0oN_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 6899.00, 333, 333);
-INSERT INTO `shop_recommended` VALUES (10, '电竞叛客RTX3060', 'https://g-search3.alicdn.com/img/bao/uploaded/i4/i1/3690358924/O1CN010WKGeb2FnFFGkwI0L_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 2799.00, 555, 555);
+INSERT INTO `shop_recommended` VALUES (1, 'MSI/微星RTX 3060', 'https://g-search3.alicdn.com/img/bao/uploaded/i4/i1/654519625/O1CN01L4xb8s2KyIt2aPRJL_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 3599.00, 999, 999, 1);
+INSERT INTO `shop_recommended` VALUES (2, '七彩虹RTX3060/3060TI', 'https://g-search2.alicdn.com/img/bao/uploaded/i4/i4/3918712328/O1CN01Dgtlcp1T4GnUp8kXu_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 3499.00, 333, 444, 2);
+INSERT INTO `shop_recommended` VALUES (3, '华硕ROG猛禽RTX3060', 'https://g-search3.alicdn.com/img/bao/uploaded/i4/i4/2210835472380/O1CN01VreFtP1TS5OMe5Eio_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 2849.00, 444, 444, 1);
+INSERT INTO `shop_recommended` VALUES (4, '七彩虹RTX3080/', 'https://g-search1.alicdn.com/img/bao/uploaded/i4/i1/2133729733/O1CN01Bd2jO02LllfAvf7vU_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 9199.00, 333, 333, 2);
+INSERT INTO `shop_recommended` VALUES (5, '影驰RTX3080/Ti', 'https://g-search1.alicdn.com/img/bao/uploaded/i4/i1/2651871058/O1CN01ytqlo91JgbmPyVHqN_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 9300.00, 444, 444, 1);
+INSERT INTO `shop_recommended` VALUES (6, '七彩虹RTX3070/Ti', 'https://g-search1.alicdn.com/img/bao/uploaded/i4/i4/2133729733/O1CN01eYLZYr2Lllf7DLy2W_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 4599.00, 555, 555, 2);
+INSERT INTO `shop_recommended` VALUES (7, '猛禽RTX3090/3090TI', 'https://g-search2.alicdn.com/img/bao/uploaded/i4/i2/654519625/O1CN01izYfVL2KyIt19oPoC_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 12999.00, 111, 1111, 1);
+INSERT INTO `shop_recommended` VALUES (8, '英伟达NVIDIA公版无锁', 'https://g-search2.alicdn.com/img/bao/uploaded/i4/i1/3971128232/O1CN01MXBMol2AgJ67nn97y_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 7299.00, 111, 111, 2);
+INSERT INTO `shop_recommended` VALUES (9, '影驰RTX3090名人堂', 'https://g-search2.alicdn.com/img/bao/uploaded/i4/i2/2681126335/O1CN01jwyrX21wfTpYDl0oN_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 6899.00, 333, 333, 1);
+INSERT INTO `shop_recommended` VALUES (10, '电竞叛客RTX3060', 'https://g-search3.alicdn.com/img/bao/uploaded/i4/i1/3690358924/O1CN010WKGeb2FnFFGkwI0L_!!0-item_pic.jpg_580x580Q90.jpg_.webp', 2799.00, 555, 555, 2);
 
 -- ----------------------------
 -- Table structure for shop_seller
@@ -131,16 +134,17 @@ DROP TABLE IF EXISTS `shop_seller`;
 CREATE TABLE `shop_seller`  (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '卖家店铺id',
   `seller_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '卖家店铺名称',
-  `seller_marl1` int(2) UNSIGNED ZEROFILL NOT NULL COMMENT '描述分',
-  `seller_mark2` int(2) UNSIGNED ZEROFILL NOT NULL COMMENT '服务分',
-  `seller_mark3` int(2) UNSIGNED ZEROFILL NOT NULL COMMENT '物流分',
+  `seller_mark1` float(1, 1) NOT NULL COMMENT '描述分',
+  `seller_mark2` float(1, 1) NOT NULL COMMENT '服务分',
+  `seller_mark3` float(1, 1) NOT NULL COMMENT '物流分',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of shop_seller
 -- ----------------------------
-INSERT INTO `shop_seller` VALUES (1, 'Kamisora官方店', 05, 05, 05);
+INSERT INTO `shop_seller` VALUES (1, 'Kamisora官方店', 4.0, 4.0, 4.0);
+INSERT INTO `shop_seller` VALUES (2, '官方自营店', 3.0, 4.0, 4.0);
 
 -- ----------------------------
 -- Table structure for sys_menu
@@ -162,7 +166,7 @@ CREATE TABLE `sys_menu`  (
   `del_flag` int NULL DEFAULT 0 COMMENT '是否删除（0未删除 1已删除）',
   `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '菜单表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '菜单表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_menu
@@ -184,7 +188,7 @@ CREATE TABLE `sys_role`  (
   `update_time` datetime(0) NULL DEFAULT NULL,
   `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_role
@@ -198,7 +202,7 @@ CREATE TABLE `sys_role_menu`  (
   `role_id` bigint NOT NULL AUTO_INCREMENT COMMENT '角色ID',
   `menu_id` bigint NOT NULL DEFAULT 0 COMMENT '菜单id',
   PRIMARY KEY (`role_id`, `menu_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_role_menu
@@ -216,7 +220,7 @@ CREATE TABLE `sys_user`  (
   `status` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '0' COMMENT '账号状态（0正常 1停用）',
   `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
   `phonenumber` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '手机号',
-  `sex` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户性别（0男，1女，2未知）',
+  `sex` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户性别',
   `avatar` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '头像',
   `user_type` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '1' COMMENT '用户类型（0管理员，1普通用户）',
   `create_by` bigint NULL DEFAULT NULL COMMENT '创建人的用户id',
@@ -225,13 +229,29 @@ CREATE TABLE `sys_user`  (
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
   `del_flag` int NULL DEFAULT 0 COMMENT '删除标志（0代表未删除，1代表已删除）',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1524762184922480641 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1524762184922480642 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES (1524761813231648769, 'Kamisora', 'NULL', '$2a$10$THVL/RRLI5kjYiaUZBqX1e0vH5OBd5sYfVMP1XBYNbWsV3kllTx3y', '0', NULL, NULL, NULL, NULL, '1', NULL, NULL, NULL, NULL, 0);
-INSERT INTO `sys_user` VALUES (1524762184922480641, 'wyf', 'NULL', '$2a$10$hFfulETdfxaBlJRj.hZtx..Fu0aQbql0nnmcdGghZbBFr2XPbXRBa', '0', NULL, NULL, NULL, NULL, '1', NULL, NULL, NULL, NULL, 0);
+INSERT INTO `sys_user` VALUES (1, 'Kamisora', '卡卡', '$2a$10$THVL/RRLI5kjYiaUZBqX1e0vH5OBd5sYfVMP1XBYNbWsV3kllTx3y', '0', '1210281722@qq.com', '15906877873', '男', NULL, '1', NULL, '2020-04-10 09:40:33', NULL, NULL, 0);
+INSERT INTO `sys_user` VALUES (2, 'wyf', 'NULL', '$2a$10$hFfulETdfxaBlJRj.hZtx..Fu0aQbql0nnmcdGghZbBFr2XPbXRBa', '0', NULL, NULL, NULL, NULL, '1', NULL, '2020-04-10 09:40:33', NULL, NULL, 0);
+
+-- ----------------------------
+-- Table structure for sys_user_addr
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_addr`;
+CREATE TABLE `sys_user_addr`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '地址id',
+  `addr` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户地址',
+  `user_id` bigint NULL DEFAULT NULL COMMENT '对应用户唯一id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_user_addr
+-- ----------------------------
+INSERT INTO `sys_user_addr` VALUES (1, '浙江省温州市永嘉县三江街道三江立体城盛景园3-3902', 1);
 
 -- ----------------------------
 -- Table structure for sys_user_role
@@ -241,7 +261,7 @@ CREATE TABLE `sys_user_role`  (
   `user_id` bigint NOT NULL AUTO_INCREMENT COMMENT '用户id',
   `role_id` bigint NOT NULL DEFAULT 0 COMMENT '角色id',
   PRIMARY KEY (`user_id`, `role_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_user_role
